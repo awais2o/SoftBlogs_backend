43520058984299838b533ae718edb6ee3f1c7113
awais2o <awaisqamar157@gmail.com>
2024-10-10 02:01:38 +0500

    fix: cors fix

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(
         callback(new Error('Not allowed by CORS'))
       }
     },
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
   })
 )
